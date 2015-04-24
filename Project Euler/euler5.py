6ca9eb06924cf7fa6f5be9606972d79adad426cf
amaldare93 <amaldare93@gmail.com>
2015-04-24 00:45:37 -0400

    Euler

---
+++ Project Euler/euler5.py
@@ -0,0 +1,16 @@
+'''
+	What is the smallest positive number that is divisible by all of the numbers from 1 to 20 ?
+'''
+def euler5(n):
+
+	num = 20
+	i = 11
+	while i < 21:
+		if num % i != 0: 
+			i = 11
+			num += 20
+		else:
+			i += 1
+	return num
+
+print(euler5(20))
