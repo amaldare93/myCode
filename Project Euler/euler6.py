f45133be462ac4901529e22134d4cfee5528c22b
amaldare93 <amaldare93@gmail.com>
2015-05-15 23:57:13 -0400

    saved

--- Project Euler/euler6.py
+++
@@ -1,9 +0,0 @@
-'''
-	Find the difference between the sum of the squares of the first n=100 natural #s and the square of the sum.
-'''
-
-def euler6(n):
-	return sum(range(1, n+1))**2 - sum([i**2 for i in range(1, n+1)])
-
-
-print(euler6(100))
